5269b6a498cf26d41c7369ef51758ab6a4d00d3b
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 19:01:42 +0700

    add generator

--- blocks/blocks_smd-rgb.js
+++ blocks/blocks_smd-rgb.js
@@ -1,21 +1,34 @@
-Blockly.Blocks['smd_rgb_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("SMD RGB");
-    this.appendValueInput("SMD_RGB_R_PIN")
-        .setCheck("Number")
-        .appendField("RED PIN");
-    this.appendValueInput("SMD_RGB_G_PIN")
-        .setCheck("Number")
-        .appendField("GREEN PIN");
-    this.appendValueInput("SMD_RGB_B_PIN")
-        .setCheck("Number")
-        .appendField("BLUE PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["smd_rgb_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("SMD RGB");
+      this.appendDummyInput()
+        .appendField("RED PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "SMD_RGB_RED_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("SMD_RGB_RED_STATE"),
+          "SMD_RGB_RED_STATE"
+        );
+      this.appendDummyInput()
+        .appendField("GREEN PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "SMD_RGB_GREEN_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("SMD_RGB_GREEN_STATE"),
+          "SMD_RGB_GREEN_STATE"
+        );
+      this.appendDummyInput()
+        .appendField("BLUE PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "SMD_RGB_BLUE_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("SMD_RGB_BLUE_STATE"),
+          "SMD_RGB_BLUE_STATE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_smd-rgb.js
+++ blocks/generators_smd-rgb.js
@@ -1,8 +1,36 @@
-Blockly.JavaScript['smd_rgb_block'] = function(block) {
-  var value_smd_rgb_r_pin = Blockly.JavaScript.valueToCode(block, 'SMD_RGB_R_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_smd_rgb_g_pin = Blockly.JavaScript.valueToCode(block, 'SMD_RGB_G_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_smd_rgb_b_pin = Blockly.JavaScript.valueToCode(block, 'SMD_RGB_B_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["smd_rgb_block"] = function(block) {
+    var text_smd_rgb_red_pin = block.getFieldValue("SMD_RGB_RED_PIN");
+    var variable_smd_rgb_red_state = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("SMD_RGB_RED_STATE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var text_smd_rgb_green_pin = block.getFieldValue("SMD_RGB_GREEN_PIN");
+    var variable_smd_rgb_green_state = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("SMD_RGB_GREEN_STATE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var text_smd_rgb_blue_pin = block.getFieldValue("SMD_RGB_BLUE_PIN");
+    var variable_smd_rgb_blue_state = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("SMD_RGB_BLUE_STATE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+	int SMD_RGB_RED_PIN = ${text_smd_rgb_red_pin}; //select the pin for the red LED
+	int SMD_RGB_BLUE_PIN = ${text_smd_rgb_blue_pin}; // select the pin for the  blue LED
+	int SMD_RGB_GREEN_PIN = ${text_smd_rgb_green_pin};// select the pin for the green LED
+	#END
+
+	#SETUP
+	  pinMode(SMD_RGB_RED_PIN, OUTPUT);
+	  pinMode(SMD_RGB_BLUE_PIN, OUTPUT);
+	  pinMode(SMD_RGB_GREEN_PIN, OUTPUT);
+	#END
+
+	${variable_smd_rgb_red_state} = SMD_RGB_RED_PIN;
+	${variable_smd_rgb_green_state} = SMD_RGB_GREEN_PIN;
+	${variable_smd_rgb_blue_state} = SMD_RGB_BLUE_PIN;
+    `;
+    return code;
+  };
